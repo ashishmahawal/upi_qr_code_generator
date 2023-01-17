357fe3e3be826ed400200d18f1f17953f1ea3735
ashishmahawal <amahawal@cs.iitr.ac.in>
2023-01-17 22:19:57 +0530

    fixed frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,14 @@ import { QRImage } from "./components/UserInput/QRImage";
 import React from "react";
 import { ImageContext } from "./storage/app-context";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
   return (
-<ImageContext.Provider value= {{img:''}}>
+<ImageContext.Provider value= {{img:'https://dummyimage.com/160x160/fff/aaa&text=QR+Code'}}>
   <UserInput/>
 </ImageContext.Provider>
 
--- frontend/src/components/UserInput/UserInput.jsx
+++ frontend/src/components/UserInput/UserInput.jsx
@@ -1,47 +1,66 @@
 import { getUPIQR } from "../../services/serverAPI";
-import React, { useState, useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { ImageContext } from "../../storage/app-context";
 import { QRImage } from "./QRImage";
+import { TextField, Button,Typography } from "@mui/material/";
+import "./UserInput.css"
+
+
 export function UserInput(props) {
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
   const [upi_adr, setUPIAdr] = useState("");
-  const [counter,refreshImage] = useState(0)
-  let ImageContext_1 = useContext(ImageContext)
+  const [counter, refreshImage] = useState(0);
+  let ImageContext_1 = useContext(ImageContext);
   const generateQR = async (event) => {
     event.preventDefault();
-    
+
     let qrData = {
       name,
       amount,
       upi_adr,
     };
     const responseData = await getUPIQR(qrData);
-    
-    let qrImage = await responseData.blob()
-    let imageUrl = URL.createObjectURL(qrImage)
-    ImageContext_1.img = imageUrl
-    refreshImage(counter+1)
-  }
-// useEffect(()=>{
 
-// },[img])
+    let qrImage = await responseData.blob();
+    let imageUrl = URL.createObjectURL(qrImage);
+    ImageContext_1.img = imageUrl;
+    refreshImage(counter + 1);
+  };
+
   return (
     <div>
-      <h1>UPI QR Code generator app!!!!</h1>
-      <form>
-        <label>Reciever's UPI Id</label>
-        <input type="text" onChange={(e) => setUPIAdr(e.target.value)} />
-        <br />
-        <label>Amount to Recieve</label>
-        <input type="text" onChange={(e) => setAmount(e.target.value)} />
-        <br />
-        <label>Reciever Name</label>
-        <input type="text" onChange={(e) => setName(e.target.value)} />
-        <br />
-        <button type="submit" onClick={generateQR}>Get QR</button>
-      </form>
-<QRImage></QRImage>
+      <Typography variant="h2" component="h3">
+      UPI QR Code generator
+</Typography> <br />
+      <div className="ashish">
+        <TextField
+          style={{ margin: "10px" }}
+          label="Reciever's UPI Id"
+          color="secondary"
+          focused
+          onChange={(e) => setUPIAdr(e.target.value)}
+        />
+        <TextField
+          style={{ margin: "10px" }}
+          label="Amount"
+          color="secondary"
+          focused
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <TextField
+          style={{ margin: "10px" }}
+          label="Name"
+          color="secondary"
+          focused
+          onChange={(e) => setName(e.target.value)}
+        /> <br />
+        <Button variant="contained" color="success" onClick={generateQR}>
+          Get QR
+        </Button>
+      </div>
+
+      <QRImage></QRImage>
     </div>
   );
 }
